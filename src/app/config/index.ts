5ef4be7b1150bf0344c03431e3844741c9de6263
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2025-01-03 19:18:29 +0800

    student-inter-model-added

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.join((process.cwd(), '.env')) });
+// Correct the path construction
+dotenv.config({ path: path.join(process.cwd(), '../.env') });
 
 export default {
   port: process.env.PORT,
